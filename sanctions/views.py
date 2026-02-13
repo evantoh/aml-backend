efabfce2c123006c9f8af39b82b99398ddd3891f
evansmwendake <e.mwenda@premiergroup.co.ke>
2026-02-13 22:37:21 +0300

    protect login

--- sanctions/views.py
+++ sanctions/views.py
@@ -8,7 +8,7 @@ from .services.unified_sanctions import UnifiedSanctionsBot
 
 from django.contrib.auth.decorators import login_required
 
-# @login_required(login_url="/login/")
+@login_required(login_url="/login/")
 def dashboard(request):
     return render(request, "dashboard.html")
 
